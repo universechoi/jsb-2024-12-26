8295a5efdcf811422d555e9c50bdb0a150db2b00
universechoi <skye.of.universe@gmail.com>
2024-12-26 13:35:01 +0900

    init

---
+++ src/main/java/com/mysite/sbb4/Sbb4Application.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbb4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mysite/sbb4/Sbb4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
